7d28a37393e4f675c137a888459bdc5668c4f5c5
Benoît Clemenceau <benoit.clemenceau@mac.com>
2022-10-18 16:37:44 -0400

    Installed Jest, switched from Babel to SWC minifier
    https://nextjs.org/docs/messages/swc-minify-enabled
    https://nextjs.org/docs/messages/swc-disabled

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-const { i18n } = require('./next-i18next.config')
-const { withContentlayer } = require('next-contentlayer')
+const { i18n } = require("./next-i18next.config");
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withContentlayer()({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'avatars.githubusercontent.com',
-    ],
+    domains: ["avatars.githubusercontent.com"],
   },
   i18n,
-})
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
